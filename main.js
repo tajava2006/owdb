c9925c61336a305d939db9e7bbba5dbc4394d9e6
tajava2006 <tajava2006@gmail.com>
2019-11-20 01:56:21 +0900

    stop

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 var express = require('express');
 var app = express();
-var db = require('./lib/db');
+
+
+
+app.use("/", express.static(__dirname + '/data'));
 
 app.get('/', function (req, res) {
-  db.query(`SELECT * FROM ring`,function(err,stations){
-    if(err){
-      throw err;
-    }
-    res.send(stations);
-  })
-
-  //res.send('Hello World!');
+  
+  res.send(`<!doctype html>
+  <html>
+  <head>
+    <title>OWDB</title>
+    <meta charset="utf-8"> 
+    <link rel="stylesheet" type="text/css" href="/style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.10.2/cytoscape.esm.js"></script>   
+  </head>
+  <body>
+    <h1>asdfzxc</h1>
+    <div id="cy"></div>
+    <script type="text/javascript" src="/code.js"></script>
+  </body>
+  </html>`);
 });
 
 app.listen(3000, function () {
